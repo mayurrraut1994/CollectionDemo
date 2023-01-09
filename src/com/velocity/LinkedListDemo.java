3de9b60f0ec9a38cbc017c791120cf2f7f590b10
mayurraut <mayurraut1994@gmail.com>
2023-01-09 10:33:52 +0530

    Added LinkedList demo code

---
+++ src/com/velocity/LinkedListDemo.java
@@ -0,0 +1,19 @@
+package com.velocity;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list =  new LinkedList<Integer>();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		
+		System.out.println(list);
+
+	}
+
+}
